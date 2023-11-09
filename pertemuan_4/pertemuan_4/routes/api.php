b2e0276ee87db600b987202963def4b9649fdfb4
Fatiahalzahra <fatiahalzahra2003@gmail.com>
2023-11-09 20:39:05 +0700

    Pertemuan 6

--- pertemuan_4/pertemuan_4/routes/api.php
+++ pertemuan_4/pertemuan_4/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnimalController;
-use App\Http\Controllers\Controller;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,20 @@ use App\Http\Controllers\Controller;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
 });
 
-#method get
-Route::get("/animals", [AnimalController::class, 'index']);
-
-#method post
-Route::post("/animals", [AnimalController::class, 'store']);
-
-#method put
+# Route animals
+Route::get('/animals', [AnimalController::class, 'index']);
+Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-#method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
 
+# Route students
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+
+# Method POST
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
