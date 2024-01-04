9df2d0e4865b2d65d5965ceeb863f88bd8d17e1b
Fatiahalzahra <114464481+Fatiahalzahra@users.noreply.github.com>
2024-01-04 17:50:31 +0700

    Delete Pertemuan_10 directory

--- Pertemuan_10/set_timeout2.js
+++
@@ -1,7 +0,0 @@
-console.log("fatiah membuka browser google chrome");
-setTimeout(() =>{
-    console.log ("downloading 1 hours... ");
-    console.log ("downloading complate ");
-}, 500);
-
-console.log("fatiah membuka youtube");
